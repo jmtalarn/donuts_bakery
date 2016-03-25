8b672c3ccf86c2b69922e284d0686511a1f4cbb9
jmtalarn@profile.es <jmtalarn@profile.es>
2016-03-25 21:43:02 +0100

    Same data than the example

--- main.js
+++ main.js
@@ -5,23 +5,57 @@ define(function (require) {
     var Donut = require('js/donut');
     require('js/scroll');
 
-  var dataset = {
+  var dataset =[
+    {
       title: "REVENUE",
       source: [{
           target: "Tablet",
-          color: "red",
-          value: [20000, 40000, 30000, 20000, 10000]
+          color: "#5cd235",
+          value: [30000, 30000, 30000, 30000]
       }, {
           target: "Smartphone",
-          color: "blue",
-          value: [10000, 20000, 20000, 60000, 80000]
+          color: "#0b6400",
+          value: [20000, 20000, 20000, 20000]
       }]
-  };
+  },
+  {
+    title: "IMPRESSIONS",
+    source: [{
+        target: "Tablet",
+        color: "#1ec4e2",
+        value: [5000000, 5000000, 5000000, 1000000, 2000000, 2000000]
+    }, {
+        target: "Smartphone",
+        color: "#074b65",
+        value: [5000000, 5000000, 5000000, 5000000, 5000000, 5000000]
+    }]
+},
+{
+  title: "VISITS",
+  source: [{
+      target: "Tablet",
+      color: "#fdbf01", //480000000
+      value: [100000000, 100000000, 100000000, 100000000, 40000000, 40000000]
+  }, {
+      target: "Smartphone",
+      color: "#d85510", //120000000
+      value: [50000000, 25000000, 25000000, 5000000, 10000000, 5000000]
+  }]
+}
+];
   //We prepare the dough
-  var mahDoughnut = new Donut(dataset);
+  var mahDoughnut = new Donut(dataset[0]);
   //We bake them
-  mahDoughnut.bake("#donut_choco")
-  mahDoughnut.bake("#donut_glace")
-  mahDoughnut.bake("#donut_cream")
+  mahDoughnut.bake("#donut_choco");
+
+  //We prepare the dough
+  var mahDoughnut = new Donut(dataset[1]);
+  //We bake them
+  mahDoughnut.bake("#donut_glace");
+
+  //We prepare the dough
+  var mahDoughnut = new Donut(dataset[2]);
+  //We bake them
+  mahDoughnut.bake("#donut_cream");
 
  });
